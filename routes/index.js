d73c21010cf9da2d237454b726eced6285752405
MenachoMarian <marian.menacho369@gmail.com>
2019-04-19 21:26:00 -0400

    parte 2, tarea 2, sin hash

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 var express = require('express');
+const user = require('../database/users');
+const USER = user.model;
+const USESCHEMA = user.schema;
+
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +10,42 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/user', async(req, res, next) => {
+  var params = req.body;
+  params["registerDate"] = new Date();
+  var user = new USER(params);
+  var result = await user.save();
+  res.status(200).json(result)
+});
+
+router.get("/user", async(req, res) => {
+  var list = await USER.find({});
+  res.status(200).json(list);
+});
+
+router.patch('/user', async(req, res, next) => {
+  var params = req.body;
+  var id = req.query.id;
+  if(id == null){
+    res.status(300).json({
+       msn: "Introducir id del usuario que desea actualizar"
+    });
+    return;
+  }
+  params["updateDate"] = new Date();
+  var result = await USER.findOneAndUpdate({_id: id},params);
+  res.status(200).json(result)
+});
+
+router.delete("/user", async(req, res) => {
+  var id = req.query.id;
+  if(id == null){
+    res.status(300).json({
+       msn: "Introducir id del usuario que desea actualizar"
+    });
+    return;
+  }
+  var result = await USER.remove({_id: id})
+  res.status(200).json(result);
+});
 module.exports = router;
